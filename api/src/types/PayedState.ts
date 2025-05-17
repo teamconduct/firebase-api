d79f8865d02b15aa98eb0059db7a4b63cd331253
Steven Kellner <steven.kellner@web.de>
2025-05-17 11:47:18 +0200

    feat: add toggled and payedTag functions to PayedState for state management

--- api/src/types/PayedState.ts
+++ api/src/types/PayedState.ts
@@ -13,5 +13,29 @@ export namespace PayedState {
         return Localization.shared.get(key => key.payedState[state]);
     }
 
+    export function toggled(state: PayedState): PayedState {
+        switch (state) {
+        case 'payed':
+            return 'notPayed';
+        case 'notPayed':
+            return 'payed';
+        }
+    }
+
+    export function payedTag(state: PayedState): { value: string; severity: 'secondary' | 'danger' } {
+        switch (state) {
+        case 'payed':
+            return {
+                value: formatted(state),
+                severity: 'secondary'
+            };
+        case 'notPayed':
+            return {
+                value: formatted(state),
+                severity: 'danger'
+            };
+        }
+    }
+
     export const builder = new ValueTypeBuilder<PayedState>();
 }
